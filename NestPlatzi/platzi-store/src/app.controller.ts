3fd91482a3e03df751708073d6a08eaeb9cfe221
MagaDev <magahr123@gmail.com>
2024-10-19 17:32:45 -0400

    My first commit

--- NestPlatzi/platzi-store/src/app.controller.ts
+++ NestPlatzi/platzi-store/src/app.controller.ts
@@ -1,50 +1,36 @@
 /* eslint-disable prettier/prettier */
-<<<<<<< HEAD
 import { Controller, Get, Param } from '@nestjs/common';
-=======
-import { Controller, Get, Param, Query} from '@nestjs/common';
->>>>>>> 4d41446a14746d9ba88a770e5383bd9911a2ea49
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): string {
-    return 'Hola Mundo';
-  }
-  @Get('nuevo')
-  newEndpoint() {
-    return 'yo soy un nuevo end point';
-  }
-  @Get('/ruta/')
-  hello() {
-    return 'con slas';
-  }
-<<<<<<< HEAD
-  // primera forma de envio del get
-  @Get('products/:productId')
-  getProducts(@Param() params: any) {
-    return `producto ${params.productId}`;
-  }
-  // segunda forma de envio del get
-  @Get('productss/:productId')
-  getProductss(@Param('productId') productId: string) {
-    return `producto ${productId}`;
-  }
-  // Buscar en una categoria especifica un producto especifico
-  @Get('categories/:id/products/:productId')
-  getCategory(@Param('productId') productId: string, @Param('id') id: string) {
-    return `product ${productId} and categoria ${id}`;
-  }
-=======
-  
-
-
-
-
-
-
->>>>>>> 4d41446a14746d9ba88a770e5383bd9911a2ea49
+    @Get()
+    getHello(): string {
+      return 'Hola Mundo';
+    }
+    @Get('nuevo')
+    newEndpoint() {
+      return 'yo soy un nuevo end point';
+    }
+    @Get('/ruta/')
+    hello() {
+      return 'con slas';
+    }
+    // primera forma de envio del get
+    @Get('products/:productId')
+    getProducts(@Param() params: any) {
+      return `producto ${params.productId}`;
+    }
+    // segunda forma de envio del get
+    @Get('productss/:productId')
+    getProductss(@Param('productId') productId: string) {
+      return `producto ${productId}`;
+    }
+    // Buscar en una categoria especifica un producto especifico
+    @Get('categories/:id/products/:productId')
+    getCategory(@Param('productId') productId: string, @Param('id') id: string) {
+      return `product ${productId} and categoria ${id}`;
+    }
 }
