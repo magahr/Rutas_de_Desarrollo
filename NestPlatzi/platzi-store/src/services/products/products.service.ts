1642c546fa78dcccd37862fe366fdcb3d3b09ec6
Magaly Hernández <magahr123@gmail.com>
2025-02-25 10:25:18 -0400

    24-02-2025 Understanding service

--- NestPlatzi/platzi-store/src/services/products/products.service.ts
+++ NestPlatzi/platzi-store/src/services/products/products.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
+import { Product} from '../../entities/product.entity';
+
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+   private products: Product[] = [{
+        id: 1,
+        name:  'Product 1',
+        description : 'bla bla',
+        price: 122,
+        image: '',
+        stock: 12,
+    }]
+
+
+
+
+
+
+
+}
