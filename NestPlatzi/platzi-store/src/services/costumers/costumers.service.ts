9e1585fe50ac8ab6f151fc5bd1e2eec90db08213
Magaly Hernández <magahr123@gmail.com>
2025-04-01 08:56:24 -0400

    01/04/2025 Creating customer entity

--- NestPlatzi/platzi-store/src/services/costumers/costumers.service.ts
+++ NestPlatzi/platzi-store/src/services/costumers/costumers.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Costumer } from '../../entities/customer.entity'
 
 @Injectable()
-export class CostumersService {}
+export class CostumersService {
+  private category: Costumer[] = [
+    { id: 1,
+      name: 'Name 1',
+      lastname: 'Lastname 1'
+    }
+    ]
+}
+
