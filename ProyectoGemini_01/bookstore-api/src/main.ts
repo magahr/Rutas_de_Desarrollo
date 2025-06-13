01180cd910801e3efa1d079e75196f52abb5256c
Magaly Hernández <magahr123@gmail.com>
2025-06-13 11:31:14 -0400

    13-06-2025 - feat: My first commint in frontend

--- ProyectoGemini_01/bookstore-api/src/main.ts
+++ ProyectoGemini_01/bookstore-api/src/main.ts
@@ -1,33 +1,38 @@
 // src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; // Importamos SwaggerModule y DocumentBuilder
-import { ValidationPipe } from '@nestjs/common'; // Asegúrate de importar ValidationPipe
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Habilitar la tubería de validación globalmente
-  // Esto es crucial para que class-validator funcione con tus DTOs
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // Elimina propiedades que no están en el DTO
-      transform: true, // Transforma los tipos de datos a los del DTO
-      forbidNonWhitelisted: true, // Lanza un error si hay propiedades no permitidas
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
     }),
   );
 
-  // --- Configuración de Swagger ---
+  // --- Configuración de CORS ---
+  app.enableCors({
+    origin: 'http://localhost:3001', // Normalmente Next.js usa el puerto 3000 por defecto en desarrollo
+                                     // se va a usar el 3001, para no tener problemas con Next
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  // --- Fin de la configuración de CORS ---
+
   const config = new DocumentBuilder()
-    .setTitle('Bookstore API') // Título de tu API
-    .setDescription('Documentación de la API para la gestión de libros en una librería.') // Descripción de tu API
-    .setVersion('1.0') // Versión de tu API
-    .addTag('books', 'Operaciones relacionadas con la gestión de libros') // Añade una etiqueta para agrupar las rutas
+    .setTitle('Bookstore API')
+    .setDescription('Documentación de la API para la gestión de libros en una librería.')
+    .setVersion('1.0')
+    .addTag('books', 'Operaciones relacionadas con la gestión de libros')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document); // 'api/docs' será la ruta donde se servirá la documentación interactiva
-  // --- Fin de la configuración de Swagger ---
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(3000);
   console.log(`Aplicación NestJS corriendo en: ${await app.getUrl()}`);
